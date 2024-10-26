215e217efb7f33bfb78cdf30d1299314bf9b614a
renatogirao <renato.sgirao@gmail.com>
2024-10-26 19:03:14 -0300

    feat: cleaning git status, uploading pod files and removing xcuserdata files

--- Pods/Target Support Files/Pods-MercadoLivreChallenge-MercadoLivreChallengeUITests/Pods-MercadoLivreChallenge-MercadoLivreChallengeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MercadoLivreChallenge_MercadoLivreChallengeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MercadoLivreChallenge_MercadoLivreChallengeUITests
-@end
