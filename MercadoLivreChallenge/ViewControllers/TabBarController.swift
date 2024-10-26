aa1f0283990c17ec686067d00606e3a2345f5823
renatogirao <renato.sgirao@gmail.com>
2024-10-26 18:28:16 -0300

    feat: creating another tabbaritem (shopping cart)

--- MercadoLivreChallenge/ViewControllers/TabBarController.swift
+++ MercadoLivreChallenge/ViewControllers/TabBarController.swift
@@ -4,7 +4,7 @@
 ////
 ////  Created by Renato Savoia Girão on 22/10/24.
 ////
-//
+
 import UIKit
 
 class TabBarController: UITabBarController {
@@ -12,22 +12,32 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tabBar.backgroundColor = .black
+        
         let homeViewController = HomeViewController()
         let favoritesViewController = FavoritesViewController()
         let settingsViewController = SettingsViewController()
+        let shoppingCartViewController = ShoppingCartViewController()
 
-        let homeViewControllerTabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
-        let favoritesViewControllerTabBarItem = UITabBarItem(title: "Favoritos", image: UIImage(systemName: "heart"), selectedImage: UIImage(systemName: "heart.fill"))
-        let settingsViewControllerTabBarItem = UITabBarItem(title: "Configurações", image: UIImage(systemName: "gearshape"), selectedImage: UIImage(systemName: "gearshape.fill"))
+        let homeViewControllerTabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        let favoritesViewControllerTabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "heart"), selectedImage: UIImage(systemName: "heart.fill"))
+        let settingsViewControllerTabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "gearshape"), selectedImage: UIImage(systemName: "gearshape.fill"))
+        let shoppingCartViewControllerTabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "cart"), selectedImage: UIImage(systemName: "cart.fill"))
         
         homeViewControllerTabBarItem.tag = 0
         favoritesViewControllerTabBarItem.tag = 1
-        settingsViewControllerTabBarItem.tag = 2
+        shoppingCartViewControllerTabBarItem.tag = 2
+        settingsViewControllerTabBarItem.tag = 3
         
         homeViewController.tabBarItem = homeViewControllerTabBarItem
         favoritesViewController.tabBarItem = favoritesViewControllerTabBarItem
+        shoppingCartViewController.tabBarItem = shoppingCartViewControllerTabBarItem
         settingsViewController.tabBarItem = settingsViewControllerTabBarItem
-        let tabBarList = [homeViewController, favoritesViewController, settingsViewController]
+        
+        self.tabBar.unselectedItemTintColor = .darkGray
+        self.tabBar.selectedImageTintColor = .lightGray
+        
+        let tabBarList = [homeViewController, favoritesViewController, settingsViewController, shoppingCartViewController]
 
         viewControllers = tabBarList.map { UINavigationController(rootViewController: $0) }
     }
