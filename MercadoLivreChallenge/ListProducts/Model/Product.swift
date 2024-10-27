1a5288fc082169f63f515206ddfd71a652d4963c
renatogirao <renato.sgirao@gmail.com>
2024-10-27 04:39:22 -0300

    Fix: Added codingKeys to conform with JSON and solve decoding error

--- MercadoLivreChallenge/ListProducts/Model/Product.swift
+++ MercadoLivreChallenge/ListProducts/Model/Product.swift
@@ -10,16 +10,31 @@ import UIKit
 struct Product: Codable {
     let id: String
     let title: String
-    let price: Double
-    let currencyId: String
-    let availableQuantity: Int
     let condition: String
-    let permalink: String
     let thumbnail: String
-    let acceptMercadoPago: Bool
-    let installments: Installments
+    let currencyId: String
+    let price: Double
+    let availableQuantity: Int
+    let permalink: String
+    let acceptsMercadoPago: Bool
     let shipping: Shipping
+    let installments: Installments
     let attributes: [Attribute]
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case condition
+        case thumbnail
+        case currencyId = "currency_id"
+        case price
+        case availableQuantity = "available_quantity"
+        case permalink
+        case acceptsMercadoPago = "accepts_mercadopago"
+        case shipping
+        case installments
+        case attributes
+    }
 }
 
 struct Installments: Codable {
@@ -27,16 +42,36 @@ struct Installments: Codable {
     let amount: Double
     let rate: Double
     let currencyId: String
+
+    private enum CodingKeys: String, CodingKey {
+        case quantity
+        case amount
+        case rate
+        case currencyId = "currency_id"
+    }
 }
 
 struct Shipping: Codable {
     let freeShipping: Bool
     let logisticType: String
     let storePickUp: Bool
+
+    private enum CodingKeys: String, CodingKey {
+        case freeShipping = "free_shipping"
+        case logisticType = "logistic_type"
+        case storePickUp = "store_pick_up"
+    }
 }
 
 struct Attribute: Codable {
+    let id: String
     let name: String
     let valueName: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case valueName = "value_name"
+    }
 }
 
