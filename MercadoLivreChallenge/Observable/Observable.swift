4cb179241e2d639c7b31b0f1543ceb3aeb2ac4f6
renatogirao <renato.sgirao@gmail.com>
2024-10-27 16:02:16 -0300

    feat: creating observable to warn when loading and loaded API response and update view

--- MercadoLivreChallenge/Observable/Observable.swift
+++ MercadoLivreChallenge/Observable/Observable.swift
@@ -5,4 +5,24 @@
 //  Created by Renato Savoia Girão on 27/10/24.
 //
 
-import Foundation
+class Observable<T> {
+    private var listeners: [(T) -> Void] = []
+    var value: T {
+        didSet {
+            notifyListeners()
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: @escaping (T) -> Void) {
+        listeners.append(listener)
+        listener(value)
+    }
+    
+    private func notifyListeners() {
+        listeners.forEach { $0(value) }
+    }
+}
